9d82c993729f9564a0717bc69de35f4a47558b24
sandroshengelia123 <sandroshengo@yahoo.com>
2025-01-12 10:22:04 +0400

    comment

---
+++ level 56/classwork/cw.py
@@ -0,0 +1,40 @@
+# palindrome
+#
+# aba
+# aba
+#
+
+# არის თუ არა სტრინგი პალინდრომი
+
+
+
+# ara
+# ara
+
+
+# hello
+# ollah
+#
+#
+
+
+
+
+# django
+# numpy
+# matplotlib
+
+
+
+
+
+# front-end
+# html
+# css
+# js
+
+
+# react
+
+
+# c++
